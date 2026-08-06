8cfb1fdab287edf5a0febfb1cfce4ab8128713aa
codebydny <313869319+codebydny@users.noreply.github.com>
2026-08-06 20:17:14 +0530

    change

---
+++ script.js
@@ -0,0 +1,15 @@
+function checkPassword(){
+
+let pass=document.getElementById("password").value;
+
+if(pass==="W"){
+
+alert("Welcome Saku ❤️");
+
+}else{
+
+document.getElementById("error").innerHTML="Wrong Password ❤️";
+
+}
+
+}
